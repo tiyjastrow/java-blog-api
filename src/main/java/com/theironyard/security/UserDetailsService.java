6ab70c7596d8064321ec9b030e0033ffebeded18
John Jastrow <jjastrow>
2017-04-13 14:38:35 -0500

    Spring Security files

---
+++ src/main/java/com/theironyard/security/UserDetailsService.java
@@ -0,0 +1,31 @@
+package com.theironyard.security;
+
+import com.ryantablada.entities.User;
+import com.ryantablada.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
